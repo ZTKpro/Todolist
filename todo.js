16f79652eed8470fbc53246af53199b19538db8e
Damian <damianszmurlowork@gmail.com>
2019-07-30 13:45:09 +0200

    done

--- todo.js
+++ todo.js
@@ -1,8 +1,22 @@
 const app = new Vue({
-el:'#app'
+el:'#app',
 data() {
     return {
-        
+        newTodo:'',
+        todos:[],
+    }
+},
+methods: {
+    addTodo(){
+     this.todos.push({
+         title:this.newTodo,
+         done:false,
+     });
+     this.newTodo = '';
+    },
+    removeTodo(todo){
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex,1);
     }
 },
 });
